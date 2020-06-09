6c0d0a85ee1a5305986630adfb5adaa987471738
Michael <mioncgp@gmail.com>
2020-06-09 09:41:38 +0400

    d

--- 09_Simon_game/Simon Game Challenge Starting Files/game.js
+++ 09_Simon_game/Simon Game Challenge Starting Files/game.js
@@ -1,2 +1,19 @@
-// start game
-// play the sound that connected with the button
+// an array of colors
+const buttonColors = ["red", "blue", "green", "yellow"];
+// game patter array to keep track of steps
+const gamePattern = [];
+// random color
+let color = buttonColors[nextSequence()];
+
+// generates a random numner
+function nextSequence() {
+  let ranNum = Math.floor(Math.random() * 4);
+  return ranNum;
+}
+$(`.${color}`).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+function playAudio(track) {
+  let sound = new Audio();
+  sound.src = `./sounds/${track}.mp3`;
+  sound.play();
+}
