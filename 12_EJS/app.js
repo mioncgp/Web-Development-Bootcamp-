96a1241af67456ca68fe702d9bd948d7ec00c58a
Michael <mioncgp@gmail.com>
2020-06-28 13:03:51 +0400

    done

--- 12_EJS/app.js
+++ 12_EJS/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
@@ -12,16 +13,7 @@ let items = ["cookie"];
 let workItems = [];
 
 app.get("/", function (req, res) {
-  let today = new Date();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
-  res.render("list", { listTitle: day, newListItems: items });
+  res.render("list", { listTitle: date(), newListItems: items });
 });
 
 app.get("/work", function (req, res) {
@@ -45,6 +37,10 @@ app.post("/", function (req, res) {
     items.push(req.body);
     res.redirect("/");
   }
+});
+
+app.get("/about", function (req, res) {
+  res.render("about");
 });
 
 app.listen(3000, function () {
