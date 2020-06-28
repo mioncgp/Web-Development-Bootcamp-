63e53f3e913e056132f400fbfc34479f5892e27b
Michael <mioncgp@gmail.com>
2020-06-28 12:15:40 +0400

    done

---
+++ 12_EJS/app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let items = ["cookie"];
+let workItems = [];
+
+app.get("/", function (req, res) {
+  let today = new Date();
+
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+
+  let day = today.toLocaleDateString("en-US", options);
+  res.render("list", { listTitle: day, newListItems: items });
+});
+
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work", newListItems: workItems });
+});
+
+app.post("/work", function (req, res) {
+  let item = req.body.newItem;
+
+  workItems.push(item);
+
+  res.redirect("/work");
+});
+
+app.post("/", function (req, res) {
+  let item = req.body.newItem;
+  if (req.body.button === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(req.body);
+    res.redirect("/");
+  }
+});
+
+app.listen(3000, function () {
+  console.log("server started on port 3000");
+});
