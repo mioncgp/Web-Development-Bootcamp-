618fa6667dd368d1f1aa5a1396d55fe43e057a2f
Michael <mioncgp@gmail.com>
2020-06-30 17:01:58 +0400

    done

---
+++ 13 Mongoose/app.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/test", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const personSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+  name: "John",
+  age: 37,
+});
+
+const person2 = new Person({
+  name: "Mike",
+  age: 27,
+});
+const person3 = new Person({
+  name: "Sana",
+  age: 33,
+});
+
+// Person.insertMany([person2, person3], function (err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Success");
+//   }
+// });
+
+Person.find(function (err, person) {
+  if (err) {
+    console.log(err);
+  } else {
+    mongoose.connection.close();
+    person.forEach((object) => {
+      console.log(object.name);
+    });
+  }
+});
